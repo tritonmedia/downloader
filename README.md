9c12310e1f68271637d3280004dbd39c21a3ba8e
Jared Allard <jaredallard@outlook.com>
2019-02-10 11:25:47 -0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# downloader
+
+Downloader is a microservice that downloads media and stores it inside a S3 compatible storage.
+
+In Triton it is the media downloader that stages downloads before they are processed by the converter
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,56 @@
+/**
+ * Download media files
+ *
+ * @author Jared Allard <jaredallard@outlook.com>
+ * @license MIT
+ * @version 1
+ */
+
+const Config = require('triton-core/config')
+const Tracer = require('triton-core/tracer').initTracer
+const dyn = require('triton-core/dynamics')
+const kue = require('kue')
+const path = require('path')
+const queue = kue.createQueue({
+  redis: dyn('redis')
+})
+const logger = require('pino')({
+  name: path.basename(__filename)
+})
+const tracer = Tracer('downloader', logger)
+
+const cleanup = async (code = 0) => {
+  return new Promise((resolve, reject) => {
+    logger.warn('cleanup() called')
+    queue.shutdown(1000, err => {
+      if (err) logger.warn('queue.shutdown errored:', err)
+      return resolve()
+    })
+  })
+}
+
+const init = async () => {
+  const config = await Config('converter')
+  const termHandler = await require('./lib/main')(config, queue, tracer)
+
+  const exit = async error => {
+    if (error && typeof error === 'object') {
+      logger.error('Unhandled exception', error.message)
+      console.log(error.stack)
+    }
+
+    logger.error(error)
+
+    await termHandler()
+    await cleanup()
+
+    process.exit(error ? 1 : 0)
+  }
+
+  process.on('SIGINT', exit)
+  process.on('unhandledRejection', exit)
+
+  logger.info('initialized')
+}
+
+init()
